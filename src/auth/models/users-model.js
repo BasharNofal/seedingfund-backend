83a981b89f8d46df8d255b0efd88113fc251b39d
BasharNofal <bashar.nofal1997@gmail.com>
2021-09-06 14:44:49 +0300

    added database models for the projects and the users

--- src/auth/models/users-model.js
+++ src/auth/models/users-model.js
@@ -1,1 +1,59 @@
-'use strict';
+"use strict";
+
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const Users = mongoose.Schema({
+	email: { type: String, required: true, unique: true, trim:true },
+	username: { type: String, required: true, unique: true, trim:true },
+	password: { type: String, required: true, trim:true },
+	userType: {
+		type: String,
+		enum: ["admin", "user"],
+		default: "user",
+	},
+});
+
+Users.virtual("token").get(function () {
+	let tokenObj = {
+		username: this.username,
+	};
+    console.log({tokenObj})
+	return jwt.sign(tokenObj, process.env.SECRET);
+});
+
+Users.pre("save", async function () {
+	if (this.isModified("password")) {
+		this.password = await bcrypt.hash(this.password, 10);
+	}
+});
+
+// Users.statics.authenticateBasic = async function (username, password) {
+// 	try {
+//         const user = await this.findOne({ username });
+// 		// console.log(user);
+// 		const valid = await bcrypt.compare(password, user.password);
+// 		if (valid) {
+// 			return user;
+// 		}
+// 		throw new Error("Invalid User");
+// 	} catch (error) {
+// 		throw new Error(error.message);
+// 	}
+// };
+
+Users.statics.authenticateWithToken = async function (token) {
+	try {
+		const parsedToken = jwt.verify(token, process.env.SECRET);
+		const user = await this.findOne({ username: parsedToken.username });
+		if (user) {
+			return user;
+		}
+	} catch (e) {
+		throw new Error(e.message);
+	}
+};
+	
+module.exports = mongoose.model("users", Users);
