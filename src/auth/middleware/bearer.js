e161366a1082b94ec58fdd9bae15b44c0ccabd11
BasharNofal <bashar.nofal1997@gmail.com>
2021-09-06 14:50:11 +0300

    added auth routes, basic and bearer auth

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const users = require('../models/users-model');
+
+module.exports = async (req, res, next) => {
+    try {
+        console.log(req.headers.authorization)
+        if(!req.headers.authorization) {
+            next('invalid login');
+            return
+        }
+        const token = req.headers.authorization.split(' ').pop();
+        const validUser = await users.authenticateWithToken(token);
+        req.user = validUser;
+        req.token = token;
+        next();
+    } catch (error) {
+        res.status(403).send('Invalid Login');
+    }
+}
